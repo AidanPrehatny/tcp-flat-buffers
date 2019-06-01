171d5be31ce9bc2e3663a7ba3b10d2e16eec9957
Aidan Prehatny <aprehatny@yahoo.com>
2019-05-31 17:22:29 -0700

    add deserialization for gucci and prada after recieving buffer from client write

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,27 @@
 import * as net from 'net';
 import { flatbuffers } from 'flatbuffers';
-import { Bar } from '../foo_generated';
+import { Commander, Gucci, CommandType, Prada } from '../foo_generated';
+
+const createCommander = (builder, cmdType, cmdOffset) => {
+    Commander.startCommander(builder);
+    Commander.addCmdType(builder, cmdType);
+    Commander.addCmd(builder, cmdOffset);
+    return Commander.endCommander(builder);
+}
+
+const createGucci = (builder) => {
+    // store a string into the flat buffer builder
+    const gucci = builder.createString('gucci');
+    Gucci.startGucci(builder);
+    Gucci.addGucci(builder, gucci);
+    return Gucci.endGucci(builder);
+}
+
+const createPrada = (builder) => {
+    Prada.startPrada(builder);
+    Prada.addPrada(builder, 20);
+    return Prada.endPrada(builder);
+}
 
 const client = net.createConnection({ port: 5060 }, () => {
     // 'connect' listener
@@ -8,12 +29,10 @@ const client = net.createConnection({ port: 5060 }, () => {
 
     // create a new flat buffer builder and allocate 0 bytes
     const builder = new flatbuffers.Builder(0);
-    // store a string into the flat buffer builder
-    const foo = builder.createString('foo');
-    // run our creator function for our Bar table
-    const bars = Bar.createBar(builder, foo);
-    // finish our build process
-    builder.finish(bars);
+    // const gucciCmd = createGucci(builder);
+    const pradaCmd = createPrada(builder);
+    const cmd = createCommander(builder, CommandType.Prada, pradaCmd);
+    Commander.finishCommanderBuffer(builder, cmd);
     // encode the flat buffer as a uInt8Array
     const gucciUint = builder.asUint8Array();
     // store our Uint8Array in a buffer to send across TCP
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,24 @@
 import * as net from 'net';
-import { Bar } from '../foo_generated';
+import * as CommanderFbs from '../foo_generated';
 import  { flatbuffers } from 'flatbuffers';
 
 const tcpServer = net.createServer((socket) => {
     socket.on('data', (data) => {
         const buf = new flatbuffers.ByteBuffer(data);
-        const str = Bar.getRootAsBar(buf);
-        console.log(str.gucci());
+        const commander = CommanderFbs.Commander.getRootAsCommander(buf);
+        
+        const unionType = commander.cmdType();
+
+        if (unionType == CommanderFbs.CommandType.Gucci) {
+            console.log('gucci');
+            const cmdGucci = commander.cmd(new CommanderFbs.Gucci()).gucci();
+            console.log(cmdGucci);
+        }
+        if (unionType == CommanderFbs.CommandType.Prada) {
+            console.log('prada');
+            const cmdPrada = commander.cmd(new CommanderFbs.Prada()).prada();
+            console.log(cmdPrada);
+        }
     });
 }).listen(5060, '127.0.0.1');
 
